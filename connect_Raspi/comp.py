624fd9f33a6301cadbec64f21833da4f80a07b2e
hatobus <s1240056@gmail.com>
2020-10-15 10:39:55 +0900

    赤べこを動かすプログラムをリファクタリングする

--- connect_Raspi/comp.py
+++ connect_Raspi/comp.py
@@ -1,35 +1,44 @@
 #! /usr/bin/python3
-import RPi.GPIO as GPIO,time,pygame.mixer as game
+import RPi.GPIO as GPIO
+import time
+import pygame.mixer as game
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(20,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(21,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(24,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(27,GPIO.OUT)
-GPIO.setup(12,GPIO.OUT)
+# pin used in mode 1
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(23, GPIO.OUT)
+
+# Pin used in mode 2
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(18, GPIO.OUT)
+
+# pin used in mode 3
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(17, GPIO.OUT)
+
+# pin used in LED flush
+GPIO.setup(12, GPIO.OUT)
+
 tmptime=30.0
 
-MUSICDIR = "/home/pi/akabeko_headban/music/"
+MUSIC_DIR = "/home/pi/akabeko_headban/music/"
+
 
 try:
     while True:
-
         if GPIO.input(20) == True:
             game.init()
-            game.music.load(MUSICDIR+'akabeko1.mp3')
+            game.music.load(MUSICDIR + 'akabeko1.mp3')
             game.music.play(1)
-            GPIO.output(23,True)
+            GPIO.output(23, True)
             intime=time.time()
             nowtime=intime
 
             while nowtime - intime < tmptime:
-                GPIO.output(12,True)
-                time.sleep(60.0/160000*2)
-                GPIO.output(12,False)
+                GPIO.output(12, True)
+                time.sleep(60.0/60000*2)
+                GPIO.output(12, False)
                 time.sleep(60.0/160000*2)
                 nowtime=time.time()
 
@@ -37,17 +46,16 @@ try:
 
         if GPIO.input(21) == True:
             game.init()
-            game.music.load(MUSICDIR+'akabeko2.mp3')
+            game.music.load(MUSICDIR + 'akabeko2.mp3')
             game.music.play(1)
-            GPIO.output(18,True)
+            GPIO.output(18, True)
             intime=time.time()
             nowtime=intime
 
             while nowtime - intime < tmptime:
-                GPIO.output(12,True)
-#		print GPIO.input(12)
+                GPIO.output(12, True)
                 time.sleep(116.0/160000*2)
-                GPIO.output(12,False)
+                GPIO.output(12, False)
                 time.sleep(116.0/160000*2)
                 nowtime=time.time()
 
@@ -57,23 +65,24 @@ try:
             game.init()
             game.music.load(MUSICDIR+'akabeko3145.mp3')
             game.music.play(1)
-            GPIO.output(17,True)
+            GPIO.output(17, True)
             intime=time.time()
             nowtime=intime
 
             while nowtime - intime < tmptime:
-                GPIO.output(12,True)
+                GPIO.output(12, True)
                 time.sleep(145.0/160000)
-                GPIO.output(12,False)
+                GPIO.output(12, False)
                 time.sleep(145.0/160000)
                 nowtime=time.time()
 
             game.music.stop()
         else:
-            GPIO.output(23,False)
-            GPIO.output(17,False)
-            GPIO.output(18,False)
+            GPIO.output(23, False)
+            GPIO.output(17, False)
+            GPIO.output(18, False)
             time.sleep(0.1)
+
 
 except KeyboardInterrupt:
     GPIO.cleanup()
