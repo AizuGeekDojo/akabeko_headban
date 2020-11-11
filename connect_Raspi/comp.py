ef6c365722afac28eefe639c2950ca9fb81e4fe0
wancom <softpiware@gmail.com>
2020-11-11 15:47:53 +0900

    Lint

--- connect_Raspi/comp.py
+++ connect_Raspi/comp.py
@@ -17,15 +17,15 @@ MOTOR = 12
 
 # music dir and names
 MUSICDIR = "/home/pi/akabeko_headban/music"
-MUSICS_NAME = ['akabeko1.mp3','akabeko2.mp3','akabeko3145.mp3']
+MUSICS_NAME = ['akabeko1.mp3', 'akabeko2.mp3', 'akabeko3145.mp3']
 
 # pin used in mode i
 for in_p, led in zip(IN, LED):
-	GPIO.setup(in_p, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.setup(led, GPIO.OUT)
+    GPIO.setup(in_p, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(led, GPIO.OUT)
 
 # music init
-MUSICS=[os.path.join(MUSICDIR,name) for name in MUSICS_NAME]
+MUSICS = [os.path.join(MUSICDIR, name) for name in MUSICS_NAME]
 
 # pin used in LED flush
 GPIO.setup(MOTOR, GPIO.OUT)
@@ -35,33 +35,33 @@ tmptime = 30.0
 game.init()
 
 try:
-	while True:
-		for in_p, led, music, bpm in zip(IN, LED, MUSICS, BPM):
-			if GPIO.input(in_p):
-				while GPIO.input(in_p):
-					continue
-				time.sleep(0.5)
-				game.music.load(music)
-				game.music.set_volume(0.3)
-				game.music.play(1)
-				GPIO.output(led, True)
-				intime=time.time()
-				intime2=time.time()
-				flg=True
-				GPIO.output(MOTOR, flg)
-				while time.time() - intime < tmptime:
-					if time.time()-intime2>=bpm/160000.0*2:
-						flg= not flg
-						intime2=time.time()
-						GPIO.output(MOTOR, flg)
-					if GPIO.input(in_p):
-						break
-				GPIO.output(MOTOR, False)
-				GPIO.output(led,False)
-				game.music.stop()
-				while GPIO.input(in_p):
-					continue
-				time.sleep(1)
+    while True:
+        for in_p, led, music, bpm in zip(IN, LED, MUSICS, BPM):
+            if GPIO.input(in_p):
+                while GPIO.input(in_p):
+                    continue
+                time.sleep(0.5)
+                game.music.load(music)
+                game.music.set_volume(0.3)
+                game.music.play(1)
+                GPIO.output(led, True)
+                intime = time.time()
+                intime2 = time.time()
+                flg = True
+                GPIO.output(MOTOR, flg)
+                while time.time() - intime < tmptime:
+                    if time.time() - intime2 >= bpm / 160000.0 * 2:
+                        flg = not flg
+                        intime2 = time.time()
+                        GPIO.output(MOTOR, flg)
+                    if GPIO.input(in_p):
+                        break
+                GPIO.output(MOTOR, False)
+                GPIO.output(led, False)
+                game.music.stop()
+                while GPIO.input(in_p):
+                    continue
+                time.sleep(1)
 
 except KeyboardInterrupt:
-	GPIO.cleanup()
+    GPIO.cleanup()
